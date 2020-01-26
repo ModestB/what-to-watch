38f753df01d08bfbf66169061f8ec8e6bb8df47f
Modestas Bujanauskas <modestas.bujanauskas@tglab.com>
2020-01-26 13:48:39 +0200

    [issue-7] delete index.scss

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import './index.scss';
 import './assets/styles/all.scss';
 import App from './containers/app/App';
 import * as serviceWorker from './serviceWorker';
