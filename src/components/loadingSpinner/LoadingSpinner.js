3d52c7bbd0175a10d8db7b23ab76119a67533d34
Modestas Bujanauskas <modestas.bujanauskas@tglab.com>
2020-02-01 11:55:14 +0200

    [issue-8] remove bs from loadingSpinner

--- src/components/loadingSpinner/LoadingSpinner.js
+++ src/components/loadingSpinner/LoadingSpinner.js
@@ -4,7 +4,7 @@ import classes from "./LoadingSpinner.module.scss";
 
 const loadingSpinner = () => {
   return (
-    <div className="d-flex justify-content-center align-items-center w-100 h-100 p-3">
+    <div className={classes.container}>
       <div className={classes.spinner}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
     </div>
   
